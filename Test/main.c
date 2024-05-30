52167b9e24254cf9f022e942c30eaaa67b7e5b1d
Mark Markov Dmitrievich <44326964+markov404@users.noreply.github.com>
2024-05-31 00:13:02 +0200

    init#4

---
+++ Test/main.c
@@ -0,0 +1,19 @@
+/*
+ * Chulyukov_221-171_Test.c
+ *
+ * Created: 12.04.2024 9:23:22
+ * Author : student
+ */ 
+
+#include <avr/io.h>
+
+//Привет друг!!!
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
